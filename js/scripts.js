20a474bbf845389120eb3210438cb9932fa8a9bb
conner <c9er99@gmail.com>
2016-01-26 11:00:05 -0800

    website is up and running

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,24 @@ function Contact(firstName,lastName) {
   this.lastName = lastName;
   this.addresses = [];
 }
+
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+  });
+});
