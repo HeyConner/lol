2da7911c2962631eca7adbaa5cf29b4224085172
conner <c9er99@gmail.com>
2016-01-26 14:10:53 -0800

    adds all passing buisness logic, and script in js

--- js/scripts.js
+++ js/scripts.js
@@ -16,11 +16,24 @@ $(document).ready(function() {
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
-    
+
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
   });
+  $(".contact").last().click(function() {
+    $("#show-contact").show();
+    $("#show-contact h2").text(newContact.fullName());
+    $(".first-name").text(newContact.firstName);
+    $(".last-name").text(newContact.lastName);  })
 });
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;}
+
+  Address.prototype.fullAddress = function() {
+    return this.street + ", " + this.city + ", " + this.state;
+  }
